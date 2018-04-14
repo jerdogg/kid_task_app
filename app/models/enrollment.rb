e12321b2c3a31d6a04d60d7ec43fe1cf56af97cc
Jeremy Bird <jbird@jbird-l.corp.instructure.com>
2018-04-14 12:49:23 -0600

    update models for enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,7 +1,17 @@
 class Enrollment < ApplicationRecord
-  belongs_to :program
-  belongs_to :task
   belongs_to :user
-
+  belongs_to :program, optional: true
+  belongs_to :task, optional: true
+  
   enum state: [:not_started, :in_progress, :finished]
+
+  validate :task_or_program_present
+
+  private 
+
+  def task_or_program_present
+    if (task_id.nil? && program_id.nil?)
+        errors.add("Must have either a TASK or a PROGRAM for an enrollment")
+    end
+  end
 end
